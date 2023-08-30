9b13b71de5a1266cd39060e89cfdc4d8e1891fa5
SatanasIdol <kpenao@unal.edu.co>
2023-08-30 15:57:22 -0500

    2

--- program.js
+++ program.js
@@ -10,11 +10,11 @@ polygon = {
     // initial rotation in radians should be kepts as PI / 12 multiples
     rotation: 0,
     // polygon edge size should
-    edge: 8,
+    edge: 1,
     // color to fill the shape
     color: 'red',
     // number of sides
-    n: 6,
+    sides: 4,
 
     randomize: function () {
       this.position = [-100 + 200 * Math.random(), -100 + 200 * Math.random()];
@@ -27,15 +27,27 @@ polygon = {
 alert('hello js template! Check you console');
 
 // 3. object usage
-function calcApotema(){
+
+polygon.perimeter = function() {
+  return this.sides * this.edge;
+}
+
+polygon.edge = 100;
+
+console.log(polygon.perimeter());
+
+  lados = polygon.sides;
   edge = polygon.edge;
-  half = edge/2;
-  return Math.sqrt((edge*edge)-(half*half));
+
+function calcApotema(){
+  ang = 2 * Math.PI/(2*lados);
+  //rad = (ang*Math.PI)/180;
+  //grad = Math.tan();
+  // console.log(grad);
+  return (edge/(2*(Math.tan(ang))));
 }
 
 function calcPolyg(){
-  lados = polygon.n;
-  edge = polygon.edge;
   perimetro = lados*edge;
   area = (perimetro*calcApotema())/2;
   return "Area: "+ area + ", Perimetro: "+ perimetro;
@@ -44,4 +56,5 @@ function calcPolyg(){
 console.log('polygon position: ' + polygon.position);
 polygon.randomize();
 console.log('polygon position: ' + polygon.position);
+console.log(calcApotema())
 console.log(calcPolyg())
